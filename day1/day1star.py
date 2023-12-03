d1c756b40c54f00c7916d77f88c0550c2b7319bd
ICEMAN\stunwin <steve.unwin@gmail.com>
2023-12-03 10:20:15 -0800

    got ittttttt

--- day1/day1star.py
+++ day1/day1star.py
@@ -6,22 +6,16 @@ import string
 
 def main():
     total = 0
-    numwords = ["oneight", "threeight", "fiveight", "nineight", "eightwo", "eighthree", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    replacements = ["18", "38", "58", "98", "82", "83", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    numwords = ["oneight", "twone", "threeight", "fiveight", "sevenine", "eightwo", "eighthree", "nineight", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    replacements = ["18", "21", "38", "58", "79", "82", "83", "98", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     fixedlist = []
     with open(sys.argv[1], "r") as input:
         list = input.read().splitlines() 
 
-    # print(list)
-   
-   #still coming up short with 53280  
     for i in list:
         for n in numwords:
             i = i.replace(n, replacements[numwords.index(n)])
         fixedlist.append(i)
-        # print(i)
-    
-    #print(fixedlist)
     
     for i in fixedlist:
         numbers = ""
