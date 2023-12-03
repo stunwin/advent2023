fba96dd6150812eff92581c69cbc026da7e21b25
ICEMAN\stunwin <steve.unwin@gmail.com>
2023-12-03 09:58:31 -0800

    added edge cases but still not working

--- day1/day1star.py
+++ day1/day1star.py
@@ -6,31 +6,32 @@ import string
 
 def main():
     total = 0
-    numwords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    replacements = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    numwords = ["oneight", "threeight", "fiveight", "nineight", "eightwo", "eighthree", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    replacements = ["18", "38", "58", "98", "82", "83", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     fixedlist = []
     with open(sys.argv[1], "r") as input:
         list = input.read().splitlines() 
 
-    print(list[:10])
+    # print(list)
    
-   #HAHAHA THIS DOESNT WORK because numbers can share a letter kill me  
+   #still coming up short with 53280  
     for i in list:
         for n in numwords:
             i = i.replace(n, replacements[numwords.index(n)])
         fixedlist.append(i)
+        # print(i)
     
-    print(fixedlist[:10])
+    #print(fixedlist)
     
-    for i in fixedlist[:10]:
+    for i in fixedlist:
         numbers = ""
         for j in i:
             if j in string.digits:
                 numbers = numbers + j
         value = numbers[0] + numbers[-1]
-        print(value)
         total += int(value)
        
     print(total)
     
 main()    
+
