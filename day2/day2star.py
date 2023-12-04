4b570b8d2bcd90c25ecc495aa8788d53adea103d
ICEMAN\stunwin <steve.unwin@gmail.com>
2023-12-04 11:10:45 -0800

    cleaned up formatting

--- day2/day2star.py
+++ day2/day2star.py
@@ -1,17 +1,12 @@
 import sys
-import string
-
-
 
 def main():
     total = 0
     
-
-    
     with open(sys.argv[1], "r") as input:
         list = input.read().splitlines()
     
-    for idx, game in enumerate(list, start=1):
+    for game in list:
         mins = {
             "red": 0,
             "green": 0,
@@ -19,22 +14,17 @@ def main():
         }
         game = game.split(": ", 1)[1]
         pulls = game.split("; ")
-        for pdx, pull in enumerate(pulls):
+        for pull in pulls:
             dice = pull.split(", ")
             mincalc(dice, mins)
         
         total += mins["blue"] * mins["green"] * mins["red"]
 
-            
-    
     print(total)
-
 
 def mincalc(dice, mins):
     for die in dice:
         if int(die.split(" ")[0]) > mins[die.split(" ")[1]]:
             mins[die.split(" ")[1]] = int(die.split(" ")[0])
-
-
-        
+       
 main()
